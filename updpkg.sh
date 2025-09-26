cdbe0813e20801f3b5d9261dbd3002cf99d47892
Chmouel Boudjnah <chmouel@chmouel.com>
2025-09-26 18:00:32 +0200

    add updpkg

---
+++ updpkg.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Author: Chmouel Boudjnah <chmouel@chmouel.com>
+set -eufo pipefail
+GITHUB_TOKEN="${GITHUB_TOKEN:-$(pass show github/chmouel-token)}"
+PROJECT="${PROJECT:-openai/codex}"
+
+latest=$(curl -H "Authorization: ${GITHUB_TOKEN}" -s https://api.github.com/repos/${PROJECT}/releases | jq -r '[.[] | select(.prerelease == false and (.tag_name | contains("nightly") | not) and (.tag_name | contains("preview") | not))][0].tag_name')
+latest=${latest#rust-}
+latest=${latest#v}
+pkgversion=$(grep '^pkgver=' PKGBUILD)
+pkgversion=${pkgversion#pkgver=}
+
+if [[ ${pkgversion} != "${latest}" ]]; then
+  echo "Updating PKGBUILD from ${pkgversion} to ${latest}"
+  sed -i "s/pkgver=${pkgversion}/pkgver=${latest}/" PKGBUILD
+else
+  echo
+  printf "\033[3;31mPKGBUILD is already up to date with version %s\033[0m\n" "${pkgversion}"
+  exit 0
+fi
+
+updpkgsums && makepkg --printsrcinfo >.SRCINFO
+makepkg -srif
+git commit -m "Bump to ${latest}" PKGBUILD .SRCINFO
+git clean -f .
